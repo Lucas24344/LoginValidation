3eb908d50c35e5e31874c8c095a4489281f2442f
Lucas Sousa <lucassousagth@gmail.com>
2024-01-27 01:53:56 -0300

    inputs ficam na cor vermelha para indicar um erro

--- validacao.js
+++ validacao.js
@@ -21,16 +21,17 @@ function confirm(event){
             document.getElementById("err").style.display='none'   
         }
         else{
+            document.getElementById('senha').style.border="1px solid rgb(251, 24, 24)";
+            document.getElementById('confirmarsenha').style.border="1px solid rgb(251, 24, 24)"
             document.getElementById('err1').style.display="none"
             document.getElementById("ok").style.display="none";
             document.getElementById("err").style.display='block';
         }
     }
     else{
-        document.getElementById("err").style.display='none'   
+        document.getElementById('senha').style.border="1px solid rgb(251, 24, 24)"
+        document.getElementById("err").style.display='none'; 
         document.getElementById("ok").style.display="none";
-        document.getElementById('err1').style.display="block"
+        document.getElementById('err1').style.display="block";
     }
-
-
 }
